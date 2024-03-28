1e094cab0257a1c4872bb883bfbe55e72d8e850f
kinotikm <cloutkevin254@gmail.com>
2024-03-28 13:11:20 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let mainNode =document.querySelector('main#main')
+mainNode.remove()
+
+let newHeader =document.createElement('h1')
+newHeader.id ='victory'
+
+
+newHeader.textContent = 'Kinoti is the champion'
